35f36bb27562c696dcae8d8ce1c08f6e2c957426
PrasannaUL <prasannareddy15b3@gmail.com>
2023-11-16 21:02:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def data_render(request):
+    data='This is our assumption'
+    d={'assumption':data}
+    return render(request,'data_render.html',context=d)
+
+
+def login(request):
+    d={'username':'prasanna','intro':'lets start'}
+    return render(request,'login.html',context=d)
